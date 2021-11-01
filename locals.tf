4f90cf42b684e6ce8598ce708e42ba3bb634daff
Harshika Gupta <gupta.harshik@northeastern.edu>
2021-11-01 01:20:01 -0400

    added ec2, rds, s3

--- locals.tf
+++ locals.tf
@@ -4,4 +4,5 @@ locals {
   enable_classiclink_dns_support   = true
   assign_generated_ipv6_cidr_block = false
   instance_tenancy                 = "default"
+  // resource_name_prefix = "${var.namespace}-${var.resource_tag_name}"
 }
--- output.tf
+++ output.tf
@@ -14,3 +14,21 @@ output "public-route-table_id" {
   value = aws_route_table.public-route-table.id
 }
 
+output "aws_security_group_id" {
+  value = aws_security_group.application.id
+}
+
+output "rds_hostname" {
+  value       = aws_db_instance.rds.address
+  description = "rds hostname"
+}
+output "rds_port" {
+  description = "RDS instance port"
+  value       = aws_db_instance.rds.port
+  sensitive   = true
+}
+output "rds_username" {
+  description = "RDS instance root username"
+  value       = aws_db_instance.rds.username
+  sensitive   = true
+}
